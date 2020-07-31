5521b7ebf9b702d792c0c7e55fa7098017016376
Matthew Ogtong <ogtongm@gmail.com>
2020-07-31 01:35:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { Joji: 'Gimme Love'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
